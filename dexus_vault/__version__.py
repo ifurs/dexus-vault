e4cd8396c11a303e6de75adcc01dae5ef5064323
ifursovych <ivan.fursovych@gmail.com>
2024-03-15 01:43:47 +0200

    refactored

---
+++ dexus_vault/__version__.py
@@ -0,0 +1,11 @@
+"""
+___  ____ _  _ _  _ ____    _  _ ____ _  _ _    ___
+|  \ |___  \/  |  | [__     |  | |__| |  | |     |
+|__/ |___ _/\_ |__| ___]     \/  |  | |__| |___  |
+"""
+
+__title__ = "dexus-vault"
+__description__ = "Synchronizer of Dex clients with secrets in Vault"
+__url__ = "https://github.com/ifurs/dexus-vault"
+__version__ = "0.1.0"
+__license__ = "Apache 2.0"
