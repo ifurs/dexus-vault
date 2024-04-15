606e424443c59e6bf94eaaef3e76437622f1f1bd
ifurs <ivan.fursovych@gmail.com>
2024-04-15 23:48:46 +0300

    bump version

--- dexus_vault/__version__.py
+++ dexus_vault/__version__.py
@@ -7,5 +7,5 @@ ___  ____ _  _ _  _ ____    _  _ ____ _  _ _    ___
 __title__ = "dexus-vault"
 __description__ = "Synchronizer of Dex clients with secrets in Vault"
 __url__ = "https://github.com/ifurs/dexus-vault"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __license__ = "Apache License 2.0"
